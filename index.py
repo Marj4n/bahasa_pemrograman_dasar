9b3839b5d820e85d239585c86e73855fa119f794
__ <83713181+Marj4n@users.noreply.github.com>
2024-11-02 04:44:36 +0000

    02/11/2024

---
+++ index.py
@@ -0,0 +1,11 @@
+# Print nama 
+nama = "kevin"
+print(nama)
+
+# Pertambahan
+sum= 2+3*7
+print(sum)
+
+# Say hello
+sayHello = input("What is your name? ")
+print("Hello "+sayHello)
---
+++ perkenalan.py
@@ -0,0 +1,10 @@
+print("hello", end=", ")
+print("Apa kabar?", end=" ")
+kabar= input()
+print(kabar)
+
+print(type(kabar))
+
+panjang = 10
+lebar = 5
+print(f'{panjang} x {lebar} = {panjang*lebar}')
